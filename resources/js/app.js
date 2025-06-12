ca3b361a4114d23481eb5d43d670b1f4b5f35b9e
Rbto-flores <roberto.floresrosas@pm.me>
2025-06-12 10:07:46 -0400

    config: change statiles and vite configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,15 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createInertiaApp, Head, Link } from '@inertiajs/vue3'
 import '../css/app.css'
-
+import Layout from './Layouts/Layout.vue'
+import { ZiggyVue } from "../../vendor/tightenco/ziggy"
 createInertiaApp({
+
+    title: (title) => `My App ${title}`,
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
         const page = pages[`./Pages/${name}.vue`]
+        page.default.layout = page.default.layout || Layout
         if (!page) {
             throw new Error(`Página no encontrada: ./Pages/${name}.vue`)
         }
@@ -14,6 +18,20 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(ZiggyVue)
+            .component('Head', Head)
+            .component('Link', Link)
             .mount(el)
+    },
+    progress: {
+
+        // The color of the progress bar...
+        color: '#fff',
+
+        // Whether to include the default NProgress styles...
+        includeCSS: true,
+
+        // Whether the NProgress spinner will be shown...
+        showSpinner: false,
     },
 })
